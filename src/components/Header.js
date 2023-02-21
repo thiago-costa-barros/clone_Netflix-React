1aa9deb0dc802520ea567001333a84e87565a7c7
Thiago Costa Barros <114679335+thiago-costa-barros@users.noreply.github.com>
2023-02-21 09:57:02 -0300

    OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ export default ({ black }) => {
     return (
         <header className={black ? 'black--container' : ''}>
             <div className="header--logo">
-                <a href="/">
+                <a href="/clone_Netflix-React/">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" />
                 </a>
             </div>
             <div className="header--user">
-                <a href="/">
+                <a href="/clone_Netflix-React/">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuário Logo" />
                 </a>
             </div>
